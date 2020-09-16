4375daf40e201de088ad1d06c40554ae5f3e500a
Likit <likit.pre@mahidol.edu>
2020-09-16 20:54:19 +0700

    Added some modules, edit the App.vue file and added a route for the health records

--- tophealth/src/router/index.js
+++ tophealth/src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/customers/:id/health-records',
+    name: 'HealthRecords',
+    component: () => import('../views/Customers/HealthRecords.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
