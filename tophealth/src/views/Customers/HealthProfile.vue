cce894a694b34e2c4ce8047f264d295702b147e4
Chavengsak Pattanaponpaiboon <changesak@gmail.com>
2020-12-03 16:36:53 +0700

    add HealthProfile view

---
+++ tophealth/src/views/Customers/HealthProfile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Nav></Nav>
+    <section class="section">
+      <div class="container">
+        <b-field label="Name">
+            <b-input v-model="name"></b-input>
+        </b-field>
+
+        <button class="button is-success">Save</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+export default {
+  name: "HealthProfile",
+  components: {
+    Nav
+  },
+  data() {
+    return {
+        name: null,
+      
+    }
+  },
+  methods: {
+  
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
