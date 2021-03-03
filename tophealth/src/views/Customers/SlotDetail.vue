7cba1d034bcdd87a13ecd063ec682fdd429e93d3
Likit Preeyanon <likit.pre@mahidol.edu>
2021-03-03 11:03:15 +0700

    added code to post booking data to the server

--- tophealth/src/views/Customers/SlotDetail.vue
+++ tophealth/src/views/Customers/SlotDetail.vue
@@ -11,7 +11,7 @@
           <div class="column">
             <div class="buttons is-grouped is-grouped-centered">
               <a class="button is-danger" @click="$router.go(-1)">Cancel</a>
-              <a class="button is-link" @click="$router.go(-1)">Confirm</a>
+              <a class="button is-link" @click="confirm()">Confirm</a>
             </div>
           </div>
         </div>
@@ -27,7 +27,33 @@ export default {
   name: "SlotDetail",
   data() {
     return {
-      slot: {}
+      slot: {},
+      profile: {}
+    }
+  },
+  methods: {
+    confirm: function () {
+      let self = this;
+      if (self.profile !== {}) {
+        axios.post('http://localhost:5000/health-service-scheduler/api/bookings/add',
+            {
+              slotId: self.$route.params['slotId'],
+              lineId: self.profile.userId,
+            }).catch((error)=>{
+            console.log(error)
+        })
+      } else {
+        self.$liff.getProfile().then((profile)=>{
+          self.profile = profile
+          axios.post('http://localhost:5000/health-service-scheduler/api/bookings/add',
+              {
+                slotId: self.$route.params['slotId'],
+                lineId: self.profile.userId,
+              }).catch((error)=>{
+              console.log(error)
+          })
+        })
+      }
     }
   },
   mounted() {
@@ -36,6 +62,16 @@ export default {
         .then((resp)=>{
           self.slot = resp.data[0]
     });
+    self.$liff.init({
+      liffId: "1655713713-L9yW3XWK"
+    }).then(()=>{
+      if(!self.$liff.isLoggedIn()) {
+        self.$liff.login()
+      }
+      self.$liff.getProfile().then((profile)=>{
+        self.profile = profile
+      })
+    })
   }
 }
 </script>
