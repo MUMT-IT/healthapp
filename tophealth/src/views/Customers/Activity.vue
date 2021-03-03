cd292e7a762af5c7bc91c4d611fbb24f8271b02b
Likit Preeyanon <likit.pre@mahidol.edu>
2021-03-03 08:38:08 +0700

    added line liff and get a user profile to display name and picture

--- tophealth/src/views/Customers/Activity.vue
+++ tophealth/src/views/Customers/Activity.vue
@@ -3,6 +3,12 @@
     <Nav></Nav>
     <section class="section">
       <div class="container">
+        <div class="has-text-centered">
+          <figure class="image is-64x64">
+            <img v-bind:src="profile.pictureUrl" class="is-rounded">
+          </figure>
+        </div>
+        <h1 class="title is-size-5 has-text-centered">Welcome {{ profile.displayName }}</h1>
         <b-field>
           <b-select placeholder="Select services" v-model="selectedActivity">
             <option
@@ -22,6 +28,7 @@
 <script>
 import Nav from '@/components/Nav.vue'
 import axios from "axios";
+
 export default {
   name: "Activity",
   components: {
@@ -30,7 +37,8 @@ export default {
   data() {
     return {
       selectedActivity: null,
-      options: []
+      options: [],
+      profile: {}
     }
   },
   methods: {
@@ -41,6 +49,16 @@ export default {
   },
   mounted() {
     let self = this;
+    self.$liff.init({
+      liffId: "1655713713-L9yW3XWK"
+    }).then(()=>{
+      if(!self.$liff.isLoggedIn()) {
+        self.$liff.login()
+      }
+      self.$liff.getProfile().then((profile)=>{
+        self.profile = profile
+      })
+    })
     axios.get('http://localhost:5000/api/v1.0/hscheduler/services').then((resp)=>{
       resp.data[0].forEach((service)=>{
         self.options.push(service);
